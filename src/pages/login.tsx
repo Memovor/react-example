919d55681c324c5f4fb6180416039bc162ac6d4c
vsersionW <liangmr0505@163.com>
2022-06-24 15:29:52 +0800

    feat: 登录界面完善

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -28,7 +28,7 @@ const Login = () => {
       const { sessionId, menu } = data
       const { username }: { username: MenuType } = values
       setStorage('sessionId', sessionId)
-      setMenuInfo( MenuInfo[username] || menu)
+      setMenuInfo(MenuInfo[username] || menu)
       setStorage('menu', MenuInfo[username] || menu)
       history.push('/home')
     }, err => {
@@ -38,45 +38,54 @@ const Login = () => {
 
   return (
     <div className="login-wrapper">
-      <Form
-        name="normal_login"
-        className="login-form"
-        onFinish={onFinish}
-      >
-        <FormattedMessage
-        id="formTitle"
+      <header className='header'>
+        <img src="http://1.202.166.1:8180/cas/images/logo.png" />
+        <span className='title'>业务组件集成管理系统</span>
+      </header>
+      <div className='container'>
+        <Form
+          name="normal_login"
+          className="login-form"
+          onFinish={onFinish}
         >
-          {(txt:string) => <h3 className="form-title">{ txt }</h3>}
-        </FormattedMessage>
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: getIntl('usernameTip') }]}
-        >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder={ getIntl('username') } />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: getIntl('passwordTip') }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder={ getIntl('password') }
-          />
-        </Form.Item>
-        <Form.Item>
           <FormattedMessage
-          id="formTitle"
+            id="formTitle"
           >
-            {(txt:string) => <Button type="primary" htmlType="submit" className="login-form-button">{ txt }</Button>}
+            {(txt: string) => <h3 className="form-title">{txt}</h3>}
           </FormattedMessage>
-          
-        </Form.Item>
-        <Form.Item>
-          <p className="tip">{`${ getIntl('username') }：admin ${ getIntl('password') }：${ getIntl('passwordContent') }  (${ getIntl('admin') })`}</p>
-          <p className="tip">{`${ getIntl('username') }：editor ${ getIntl('password') }：${ getIntl('passwordContent') }  (${ getIntl('editor') })`}</p>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: getIntl('usernameTip') }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder={getIntl('username')} />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: getIntl('passwordTip') }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder={getIntl('password')}
+            />
+          </Form.Item>
+          <Form.Item>
+            <FormattedMessage
+              id="formTitle"
+            >
+              {(txt: string) => <Button type="primary" htmlType="submit" className="login-form-button">{txt}</Button>}
+            </FormattedMessage>
+
+          </Form.Item>
+          <Form.Item>
+            <p className="tip">{`${getIntl('username')}：admin ${getIntl('password')}：${getIntl('passwordContent')}  (${getIntl('admin')})`}</p>
+            <p className="tip">{`${getIntl('username')}：editor ${getIntl('password')}：${getIntl('passwordContent')}  (${getIntl('editor')})`}</p>
+          </Form.Item>
+        </Form>
+      </div>
+      <footer className='footer'>
+          © 2022 vsersionW 版权所有
+      </footer>
     </div>
   )
 }
